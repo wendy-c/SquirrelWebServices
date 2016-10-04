b4bdec4d5594629f2be557f40a89f7f919989467
mwongka <mikekawong@gmail.com>
2016-10-03 21:21:33 -0700

    implement dropdown feature to add to friends lists!

--- client/app/home/inputBar/inputBarContainer.js
+++ client/app/home/inputBar/inputBarContainer.js
@@ -13,13 +13,18 @@ class InputBarContainer extends React.Component {
     }
   }
   componentWillReceiveProps(props){
-    if(props.userId){
+    if(props.userId && props.userId.fbid !== this.state.userId) { 
      this.setState({
       userId: props.userId.fbid,
       assignee: props.userId.fbid,
       owner: props.userId.fbid,
      })
     }
+  }
+
+  shouldComponentUpdate(props, state){
+    console.log(state, 'what\'s in next state' )
+    return true;
   }
 
   handleInputChange(e) {
@@ -44,9 +49,13 @@ class InputBarContainer extends React.Component {
     // })
   }
 
+  setOwner(owner){
+    this.setState({owner: owner});
+  }
+
   render() {
     return (
-      <InputBarPresentational onSubmit={this.handleSubmit.bind(this)} onChange={this.handleInputChange.bind(this)}/>
+      <InputBarPresentational friends={this.props.friends} setOwner={this.setOwner.bind(this)} onSubmit={this.handleSubmit.bind(this)} onChange={this.handleInputChange.bind(this)}/>
     )
   }
 }
