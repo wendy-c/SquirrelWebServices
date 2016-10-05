9560084c2fe2cf151a33392b6b09b86a9fcbdf87
mwongka <mikekawong@gmail.com>
2016-10-05 16:39:15 -0700

    creat signup flow to work with newly created DBservice signup API

--- client/app/login/LoginPresentational.js
+++ client/app/login/LoginPresentational.js
@@ -3,6 +3,7 @@ import React, { PropTypes } from 'react';
 const LoginPresentational = (props) => {
     return (
       <div>
+      <div>
         <h3 className="header">Save Articles, Read Offline, Share with Friends</h3>
         <br />
         <br />
@@ -14,58 +15,26 @@ const LoginPresentational = (props) => {
         <br />
         <div className="loginBody">
 
-        <div className='buttoncontainer'>
-          <h5 className="sub-head">Login</h5>
-          <br />
-          <div className="loginBody">
-          <div className='row center'>
-            <form style={{color: 'white'}} id='loginform' onSubmit={props.handleSubmit}>
-            Username: <input onChange={props.getRefUsername} className='form-control' type='text' placeholder='Enter username'></input>
-            Password: <input onChange={props.getRefPassword} className='form-control' type='text' placeholder='Enter password'></input>
-              <h6 style={{color: 'white'}} >{props.message}</h6>
-              <button type='submit' className='btn'>Submit</button>
+          <div className="row center">
+            <form id='loginform' onSubmit={props.handleSubmit}>
+                Username: <input onChange={props.getRefUsername} className='form-control' type='text' placeholder='Enter username'></input>
+                Password: <input onChange={props.getRefPassword} className='form-control' type='text' placeholder='Enter password'></input>
+                <h5>{props.message}</h5>
+                <button type='submit' className='btn'>Submit</button>
             </form>
-          </div> 
-            <div className="row center">
-              <button className="downloadBtn">
-                <h5>Download</h5>
-                <h6>Squirrel beta 1.0</h6>
-              </button>
-            </div>
+          </div>
+          <div className="row center">
+            <button className="downloadBtn">
+              <h5>Download</h5>
+              <h6>Squirrel beta 1.0</h6>
+            </button>
           </div>
         </div>
+      </div>
       </div>
       );
 };
 
 export default LoginPresentational;
-
-// <div>
-//   <div>
-//     <h3 className="header">Save Articles, Read Offline, Share with Friends</h3>
-//     <img className="bk-image" src="styles/img/background-wood.png"/>
-//   </div>
-
-//   <div className='buttoncontainer'>
-//     <h5 className="sub-head">Login</h5>
-//     <br />
-//     <div className="loginBody">
-//     <div className='row center'>
-//       <form style={{color: 'white'}} id='loginform' onSubmit={props.handleSubmit}>
-//       Username: <input onChange={props.getRefUsername} className='form-control' type='text' placeholder='Enter username'></input>
-//       Password: <input onChange={props.getRefPassword} className='form-control' type='text' placeholder='Enter password'></input>
-//         <h6 style={{color: 'white'}} >{props.message}</h6>
-//         <button type='submit' className='btn'>Submit</button>
-//       </form>
-//     </div> 
-//       <div className="row center">
-//         <button className="downloadBtn">
-//           <h5>Download</h5>
-//           <h6>Squirrel beta 1.0</h6>
-//         </button>
-//       </div>
-//     </div>
-//   </div>
-// </div>
 
 
